a362e97d947ccb27f82b391d97eb3b3e331ee0ed
vicky-ogutu <vikogutu77@gmail.com>
2026-02-02 04:17:53 +0300

    Fix query in AccountController

--- MobileWalletSpringBootAPI/src/main/java/com/comulynx/wallet/rest/api/controller/AccountController.java
+++ MobileWalletSpringBootAPI/src/main/java/com/comulynx/wallet/rest/api/controller/AccountController.java
@@ -7,12 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.comulynx.wallet.rest.api.AppUtilities;
 import com.comulynx.wallet.rest.api.exception.ResourceNotFoundException;
@@ -25,8 +20,8 @@ import com.google.gson.JsonObject;
 @RestController
 @RequestMapping("/api/v1/accounts")
 public class AccountController {
-	private static final Logger logger = LoggerFactory.getLogger(AccountController.class);
 
+	private static final Logger logger = LoggerFactory.getLogger(AccountController.class);
 	private Gson gson = new Gson();
 
 	@Autowired
@@ -37,28 +32,27 @@ public class AccountController {
 		return accountRepository.findAll();
 	}
 
-	
 	@PostMapping("/balance")
 	public ResponseEntity<?> getAccountBalanceByCustomerIdAndAccountNo(@RequestBody String request)
 			throws ResourceNotFoundException {
+
 		try {
+			JsonObject req = gson.fromJson(request, JsonObject.class);
 			JsonObject response = new JsonObject();
 
-			final JsonObject balanceRequest = gson.fromJson(request, JsonObject.class);
-			String customerId = balanceRequest.get("customerId").getAsString();
+			String customerId = req.get("customerId").getAsString();
 
-
-			// TODO : Add logic to find Account balance by CustomerId
-			Account account = null;
+			Account account = accountRepository.findAccountByCustomerId(customerId)
+					.orElseThrow(() -> new ResourceNotFoundException("Account not found"));
 
 			response.addProperty("balance", account.getBalance());
 			response.addProperty("accountNo", account.getAccountNo());
-			return ResponseEntity.ok().body(gson.toJson(response));
+
+			return ResponseEntity.ok(gson.toJson(response));
+
 		} catch (Exception ex) {
-			logger.info("Exception {}", AppUtilities.getExceptionStacktrace(ex));
-
+			logger.error("Exception {}", AppUtilities.getExceptionStacktrace(ex));
 			return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-
 		}
 	}
 }
